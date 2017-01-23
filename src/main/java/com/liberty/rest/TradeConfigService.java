589be66cf905e33dcff0e052cf6233817c43911d
Dmytro Kovalskyi <dmytro_kovalskyi@epam.com>
2017-01-23 18:13:48 +0200

    Added swagger

--- src/main/java/com/liberty/rest/TradeConfigService.java
+++ src/main/java/com/liberty/rest/TradeConfigService.java
@@ -1,10 +1,12 @@
 package com.liberty.rest;
 
+import com.liberty.common.StringConstants;
 import com.liberty.model.PlayerId;
 import com.liberty.model.PlayerTradeStatus;
 import com.liberty.rest.request.AutoBuyRequest;
 import com.liberty.services.UserTradeConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -15,28 +17,33 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/api/trade/config")
+@PreAuthorize(StringConstants.SECURED_ROLES_EXPRESSION)
 public class TradeConfigService {
 
     @Autowired
     private UserTradeConfigService tradeConfigService;
 
 
+    @PreAuthorize(StringConstants.SECURED_ROLES_EXPRESSION)
     @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
     public void removePlayer(@PathVariable PlayerId id) {
         tradeConfigService.deletePlayerToAutoBuy(id);
     }
 
+    @PreAuthorize(StringConstants.SECURED_ROLES_EXPRESSION)
     @RequestMapping(method = RequestMethod.GET)
     public Map<PlayerId, PlayerTradeStatus> getAll() {
         return  tradeConfigService.getAllTrades();
     }
 
 
+    @PreAuthorize(StringConstants.SECURED_ROLES_EXPRESSION)
     @RequestMapping(path = "/autobuy/player", method = RequestMethod.POST)
     public void updateAutoBuy(@RequestBody AutoBuyRequest request) {
         tradeConfigService.enablePlayer(request.getPlayerId(), request.getEnabled());
     }
 
+    @PreAuthorize(StringConstants.SECURED_ROLES_EXPRESSION)
     @RequestMapping(path = "/player/update", method = RequestMethod.POST)
     public void updatePlayer(@RequestBody PlayerTradeStatus request) {
         tradeConfigService.addPlayerToAutoBuy(request);
